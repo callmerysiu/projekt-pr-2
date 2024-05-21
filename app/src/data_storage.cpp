25c0b5ae98537d27baf2ae0218aa51ae53d63870
callmerysiu <jakubryszczyk+git@gmail.com>
2024-05-21 14:03:26 -0700

    it compiles now

--- app/src/data_storage.cpp
+++ app/src/data_storage.cpp
@@ -1,6 +1,10 @@
 #include "data_storage.h"
 
-
-Transaction* MockDatabase::get_user_transactions(int id){
-    return 
+// Transaction::Transaction()
+list<Transaction> MockDatabase::get_user_transactions(int id)
+{
+    time_t time = 0;
+    Transaction t = Transaction(1, 1, time);
+    list<Transaction> transaction_list = {t};
+    return transaction_list;
 }
