1a3a3eff636c06446ed29e5b8eaedfc364735d2e
callmerysiu <jakubryszczyk+git@gmail.com>
2024-05-21 12:42:12 -0700

    add transaction prototype

--- app/src/data_storage.h
+++ app/src/data_storage.h
@@ -1,3 +1,26 @@
-class IDataStorage{};
+#include <ctime>
+#include <iostream>
+
+using namespace std;
+
+class Transaction
+{
+private:
+    int value;
+    bool income; // jezli true to znaczy ze to przychod jest
+    time_t date;
+public:
+    Transaction(int value, bool income, time_t date);
+    ~Transaction();
+};
+
+
+class IDataStorage{
+    public:
+        virtual Transaction* get_user_transactions(int user_id);
+        virtual void store_user_transaction(int user_id, Transaction thing_to_store);
+
+        virtual Transaction* get_user_transactions_from(int user_id);
+};
 
 class MockDatabase : public IDataStorage{};
